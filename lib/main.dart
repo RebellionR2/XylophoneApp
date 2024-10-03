5a191860604c549250965c4e2653d4d83cc97342
arsenal-game <arsenalgame579@gmail.com>
2024-10-03 22:52:23 +0530

    Initial commit for  project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
-
+import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  @override
+  Future<void> playSound(int n) async {
+      final player = AudioPlayer();
+      await player.play(AssetSource('note$n.wav'));
+  }
+
+  Expanded keyNote({required Color color,required int sound}){
+     return Expanded(
+       child: TextButton(
+                  onPressed: () async {
+                    playSound(sound);
+                  },
+                  child: SizedBox.shrink(),
+                  style: TextButton.styleFrom(
+                      backgroundColor: color,
+                      minimumSize: Size(50, 50),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
+                ),
+     );
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+             
+              keyNote(color:Colors.red, sound:1),
+              keyNote(color: Colors.blue, sound:2),
+              keyNote(color: Colors.green, sound:3),
+              keyNote(color: Colors.yellow, sound:4),
+              keyNote(color: Colors.pink, sound:5),
+              keyNote(color: Colors.grey, sound:6),
+              keyNote(color: Colors.black, sound:7)   
+            ],
+          ),
+          
+         
         ),
       ),
     );
